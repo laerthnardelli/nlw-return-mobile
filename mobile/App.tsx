1855201e764a3ce9e04acb525f630881abc13e3f
laerthnardelli <laerthnardelli@gmail.com>
2022-05-07 16:28:37 -0300

    added function to Open and Close BottomSheet

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,3 +1,5 @@
+import "react-native-gesture-handler";
+
 import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { View, StyleSheet } from "react-native";
@@ -9,7 +11,7 @@ import {
   Inter_500Medium,
 } from "@expo-google-fonts/inter";
 
-import { Widget } from "./src/components/Widget";
+import Widget from "./src/components/Widget";
 import { theme } from "./src/theme";
 
 export default function App() {
@@ -26,8 +28,8 @@ export default function App() {
   SplashScreen.hideAsync();
   return (
     <View style={styles.container}>
-      <Widget />
       <StatusBar style="light" backgroundColor="transparent" translucent />
+      <Widget />
     </View>
   );
 }
